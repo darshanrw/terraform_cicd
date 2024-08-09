5d84ad19d567cde5b3f6d769da97ad81dc32705b
Darshan Rajendra Wadekar <Dwadekar4575@conestogac.on.ca>
2024-08-09 19:23:59 -0400

    Add Terraform CI/CD pipeline

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"
+}
+    
+resource "aws _instance" "example" {
+ami = "ami-0c55b159cbfafe1f0"
+instance_type = "t2.micro"
+count = var.core_count
+
+tags = {
+    Name = "group2_instance"
+}
+}
