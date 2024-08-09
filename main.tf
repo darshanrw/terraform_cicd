3f59cd1bb7488e9505c89855b3b14b90c9bba83c
Darshan Rajendra Wadekar <Dwadekar4575@conestogac.on.ca>
2024-08-09 19:32:56 -0400

    errors solved

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
-    
-resource "aws _instance" "group2_instance" {
-ami = "ami-0c55b159cbfafe1f0"
-instance_type = "t2.micro"
-count = var.core_count
 
-tags = {
+resource "aws_instance" "group2_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  count         = var.core_count
+
+  tags = {
     Name = "group2_instance"
-}
+  }
 }
