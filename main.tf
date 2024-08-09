ff9984646cf20e483f5b28e1374706067d8c708b
Darshan Rajendra Wadekar <Dwadekar4575@conestogac.on.ca>
2024-08-09 19:29:22 -0400

    instance renamed

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
     
-resource "aws _instance" "example" {
+resource "aws _instance" "group2_instance" {
 ami = "ami-0c55b159cbfafe1f0"
 instance_type = "t2.micro"
 count = var.core_count
